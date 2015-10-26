33b4dff0170cdcf1bbed934e78b1a9f14bc90125
WMDE-Fisch <christoph.fischer@wikimedia.de>
2015-10-26 14:35:38 +0100

    more consistent renaming and naming

--- features/step_definitions/banner_steps.rb
+++ features/step_definitions/banner_steps.rb
@@ -9,7 +9,7 @@ When(/^I am on a random mobile Wikipedia article page and provide a (.*)$/) do |
   on(ArticlePage).goto_random_page_with_banner(banner_code, true)
 end
 
-Given(/^I am on a random Wikipedia article page and provide a (.*)$/) do | banner_code |
+When(/^I am on a random Wikipedia article page and provide a (.*)$/) do | banner_code |
   on(ArticlePage).goto_random_page_with_banner(banner_code, false)
 end
 
@@ -17,33 +17,33 @@ When(/^I am on a long Wikipedia article page and provide a (.*)$/) do | banner_c
   on(ArticlePage).goto_article_page_with_banner('Stausee Lipno', banner_code)
 end
 
-Then(/^The (.*) should be present$/) do | banner_div_id |
-  expect(on(ArticlePage).get_element(banner_div_id).exists?).to be true
+When(/^I start a timer$/) do
+  @start_time = Time.now
 end
 
-And(/^I start a timer$/) do
-  @start_time = Time.now
+When(/^I wait a second$/) do
+  sleep(1)
+end
+
+When(/^I click the (.*) element$/) do | close_button |
+  on(ArticlePage).get_element(close_button).click
+end
+
+When(/^I click the (.*) element span$/) do | close_button |
+  on(ArticlePage).get_span_by_class(close_button).click
+end
+
+Then(/^The (.*) should be present$/) do | banner_div_id |
+  expect(on(ArticlePage).get_element(banner_div_id).exists?).to be true
 end
 
 Then(/^The (.*) should become visible$/) do |banner_div_id|
   on(ArticlePage).wait_for_banner_to_show(banner_div_id)
 end
 
-And(/^The timer should not exceed the (.*)$/) do | time_limit |
+Then(/^The timer should not exceed the (.*)$/) do | time_limit |
   duration = Time.now - @start_time
   duration.should < time_limit.to_i
-end
-
-And(/^I wait a second$/) do
-  sleep(1)
-end
-
-And(/^I click the (.*) element$/) do | close_button |
-  on(ArticlePage).get_element(close_button).click
-end
-
-And(/^I click the (.*) element span$/) do | close_button |
-  on(ArticlePage).get_span_by_class(close_button).click
 end
 
 Then(/^The (.*) should hide$/) do | banner_div_id |
