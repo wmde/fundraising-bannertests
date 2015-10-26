a5261501c426265f37b85fd026a7481a3f63d4e0
WMDE-Fisch <christoph.fischer@wikimedia.de>
2015-10-26 14:12:05 +0100

    updated to mediawiki_selenium 1.5

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,6 @@
 require 'mediawiki_selenium'
+require 'mediawiki_selenium/support'
+require 'mediawiki_selenium/step_definitions'
 
 require 'net/http'
 require 'active_support/all'
@@ -6,10 +8,11 @@ require 'require_all'
 
 config = YAML.load_file('config/config.yml')
 config.each do |k, v|
-  unless ENV["#{k}"]
-    ENV["#{k}"] = "#{v}"
-  end
+  ENV["#{k}"] = "#{v}" unless ENV["#{k}"]
 end
 
+# require_all 'features/support/utils'
 require_all 'features/support/modules'
 require_all 'features/support/pages'
+
+PageObject.default_element_wait = 10 # increased to avoid fails
